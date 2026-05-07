4727753592ba434722f4e360dc37b97c8e5700b4
astrodeveloper10 <206012923+astrodeveloper10@users.noreply.github.com>
2026-05-07 14:03:40 +0530

    add solution_hashset to 349-intersection-of-two-arrays

---
+++ problems/349-intersection-of-two-arrays/solution_hashset.py
@@ -0,0 +1,31 @@
+"""
+Given two integer arrays nums1 and nums2, return an array of their intersection. 
+Each element in the result must be unique and you may return the result in any order.
+
+Example 1:
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+
+Example 2:
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Explanation: [4,9] is also accepted.
+
+Constraints:
+1 <= nums1.length, nums2.length <= 1000
+0 <= nums1[i], nums2[i] <= 1000
+"""
+class Solution:
+    def intersection(self, nums1: list[int], nums2: list[int]) -> list[int]:
+        nums1_set = set(nums1)
+        nums2_set = set(nums2)
+        res = []
+
+        for num in nums1_set:
+            if num in nums2_set:
+                res.append(num)
+        
+        return res
+
+s = Solution()
+print(s.intersection([1,2,2,1], [2, 2]))
