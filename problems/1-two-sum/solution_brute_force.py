7c9ae31b49b67fc8e2bb6e16b685bd0a5eeb7586
astrodeveloper10 <206012923+astrodeveloper10@users.noreply.github.com>
2026-05-22 21:06:28 +0530

    update comments for 1-two-sum

--- problems/1-two-sum/solution_brute_force.py
+++ problems/1-two-sum/solution_brute_force.py
@@ -1,8 +1,8 @@
 """
-Given an array of integers nums and an integer target, 
+Given an array of integers nums and an integer target,
 return indices of the two numbers such that they add up to target.
 
-You may assume that each input would have exactly one solution, 
+You may assume that each input would have exactly one solution,
 and you may not use the same element twice.
 
 You can return the answer in any order.
@@ -28,7 +28,38 @@ Only one valid answer exists.
 
 Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
 """
+
+
 class Solution:
+    # we need indices of the two numbers that add up to target
+    # so we should use either range or enumerate in order to get index
+
+    # how do we check if two numbers equal to a target ?
+    # Use two loops
+    # First loop tracks i (0, n)
+    # Second loop tracks j (i + 1 to n)
+    # add them and see if they are equal
+    # if nums[i] + num[j] == target:
+    #   return [i, j]
+
+    # we may not use the same element twice
+    # "Just to clarify—when you say 'may not use the same element twice,'
+    # do you mean I can't use the same index twice, or I can't use duplicate
+    # values? For example, if the array is [3, 3] and target is 6, would
+    # returning [0, 1] be valid?"
+
+    # i = 0, j = 1, 2, 3, ...
+    # i = 1, j = 2, 3, 4, ...
+
+    # j = i + 1 avoids using the same element twice as
+    # j is always greater than i
+
+    # Problem with this solution:
+    # We use two loops
+    # First loop iterates from 0 to n
+    # Second loop iterates from 1 to n
+    # Worst case time complexity: O(n^2)
+    # Can we improve our solution ?
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         n = len(nums)
 
@@ -36,9 +67,9 @@ class Solution:
             for j in range(i + 1, n):
                 if nums[i] + nums[j] == target:
                     return [i, j]
-        
+
         return []
 
 
 s = Solution()
-print(s.twoSum([2,7,11,15], 9))
+print(s.twoSum([2, 7, 11, 15], 9))
--- problems/1-two-sum/solution_hashmap.py
+++ problems/1-two-sum/solution_hashmap.py
@@ -1,8 +1,8 @@
 """
-Given an array of integers nums and an integer target, 
+Given an array of integers nums and an integer target,
 return indices of the two numbers such that they add up to target.
 
-You may assume that each input would have exactly one solution, 
+You may assume that each input would have exactly one solution,
 and you may not use the same element twice.
 
 You can return the answer in any order.
@@ -28,9 +28,11 @@ Only one valid answer exists.
 
 Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
 """
+
+
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        diff_map = {}
+        map = {}
 
         for index, num in enumerate(nums):
             # Calculate the diff
@@ -39,14 +41,14 @@ class Solution:
 
             # Check if diff is present in the map
             # If yes, then return the current index and the index of diff (fetch it from the map)
-            if diff in diff_map:
-                return [diff_map[diff], index]
+            if diff in map:
+                return [map[diff], index]
 
             # Add the current num and its index to the map
-            diff_map[num] = index
-        
+            map[num] = index
+
         return []
 
 
 s = Solution()
-print(s.twoSum([2,7,11,15], 9))
+print(s.twoSum([2, 7, 11, 15], 9))
