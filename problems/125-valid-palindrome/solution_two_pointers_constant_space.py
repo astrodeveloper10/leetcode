dab523ca083a0d3830ff6f19e75c627bea898aa2
astrodeveloper10 <206012923+astrodeveloper10@users.noreply.github.com>
2026-06-01 16:17:01 +0530

    add solution_two_pointers_constant_space for 125-valid-palindrome

--- problems/125-valid-palindrome/solution_two_pointers_constant_space.py
+++ problems/125-valid-palindrome/solution_two_pointers_constant_space.py
@@ -1,6 +1,6 @@
 """
-A phrase is a palindrome if, after converting all uppercase letters into lowercase letters 
-and removing all non-alphanumeric characters, it reads the same forward and backward. 
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters
+and removing all non-alphanumeric characters, it reads the same forward and backward.
 
 Alphanumeric characters include letters and numbers.
 
@@ -27,29 +27,29 @@ Constraints:
 1 <= s.length <= 2 * 105
 s consists only of printable ASCII characters.
 """
+
+
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = s.lower()
-        s_updated = []
-
-        # reconstructing a string
-        for ch in s:
-            if ch.isalnum():
-                s_updated.append(ch)
-        
         left = 0
-        right = len(s_updated) - 1
+        right = len(s) - 1
 
         while left < right:
-            if s_updated[left] != s_updated[right]:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+
+            if s[left].lower() != s[right].lower():
                 return False
-            
+
             left += 1
             right -= 1
-        
+
         return True
 
 
 s = Solution()
 print(s.isPalindrome("A man, a plan, a canal: Panama"))
-
+print(s.isPalindrome(".       "))
+print(s.isPalindrome("race a car"))
